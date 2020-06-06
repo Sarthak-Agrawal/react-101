149b8763db5d2d2323b9d6981c7400680e5235bc
saragrawal <saragrawal@expedia.com>
2020-06-07 03:01:12 +0530

    Basic React complete: Basic  Tic tac toe finish

--- src/index.js
+++ src/index.js
@@ -27,18 +27,52 @@ function Square(props) {
 //     }
 //   }
   
+  function calculateWinner (squares) {
+      const lines = [
+          [0, 1, 2],
+          [3, 4, 5],
+          [6, 7, 8],
+          [0, 3, 6],
+          [1, 4, 7],
+          [2, 5, 8],
+          [0, 4, 8],
+          [2, 4, 6]
+      ];
+
+      for(let i=0;i<lines.length;i++) {
+          const [a,b,c] = lines[i];
+          if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+              console.log(squares[a]);
+              return squares[a];
+          }
+      }
+
+      return null;
+  }
+
   class Board extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            squares : Array(9).fill(null)
+            squares : Array(9).fill(null),
+            xIsNext : true
         }
     }
 
     handleClick(i) {
         const tempSquares = this.state.squares.slice(); //Used Imutabiility
-        tempSquares[i] = 'X';
-        this.setState({squares: tempSquares});
+        if (calculateWinner(tempSquares) || tempSquares[i]) {
+            return;
+        }
+        if (this.state.xIsNext) {
+            tempSquares[i] = 'X';
+        } else {
+            tempSquares[i] = 'O';
+        }
+        this.setState({
+            squares: tempSquares,
+            xIsNext: !this.state.xIsNext
+        });
     }
 
     renderSquare(i) {
@@ -49,7 +83,13 @@ function Square(props) {
     }
   
     render() {
-      const status = 'Next player: X';
+      const winner =  calculateWinner(this.state.squares);
+      let status;
+      if (winner) {
+          status = "Winner: " + winner;
+      } else {
+          status = 'Next player: ' + (this.state.xIsNext? 'X' : 'O');
+      }
   
       return (
         <div>
