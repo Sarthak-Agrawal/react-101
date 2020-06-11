f20175726332858911d7296be1ec09389ffaa542
saragrawal <saragrawal@expedia.com>
2020-06-12 02:53:06 +0530

    Added Sass support

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+// import './index.css';
+import './css/main.css';
 import Clock from './timer';
 import Button from './statefulButton';
 import NameForm from './form';
