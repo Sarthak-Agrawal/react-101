bff3fc5f51982be883af8100a03b89486c06cfbc
saragrawal <saragrawal@expedia.com>
2020-06-16 02:14:00 +0530

    Rev 1

---
+++ src/todo/todoList.tsx
@@ -0,0 +1,51 @@
+// import * as React from 'react';
+// import TodoItem, {TodoItemProps} from './todoItem';
+
+// interface TodoListState {
+//     items: React.ReactElement<TodoItemProps>[];
+// }
+
+// function TodoList(props:{}) {
+//     const[todoList, updateList] = React.useState<TodoListState> ({
+//         items: (id: number) => {
+//             return 
+//         } 
+//     });
+
+//     function addItem() {
+//         todoList.push(<TodoItem key={todoList.length} id={todoList.length} />);
+//         return todoList;
+//     })
+
+//     return (
+//         <div className="todo-list">
+//                 {todoList}
+//                 <button onClick= {updateList(addItem)}>
+//                     Add Item
+//                 </button>
+//         </div>
+//     )
+// }
+
+import * as React from 'react';
+const TodoItem = require('./todoItem');
+
+
+function TodoList(props:{}) {
+ // const initialList: Array<typeof TodoItem> = [];
+ // const[todoList, updateList] = React.useState(initialList);
+  const[todoList, updateList] = React.useState(Array<typeof TodoItem>());
+
+  return (
+      <div className="todo-list">
+        {todoList}
+        <button onClick= {() => updateList(
+           [...todoList, <TodoItem id={todoList.length} />]
+        )}>
+          Add Item
+        </button>
+      </div>
+  )
+}
+
+export default TodoList;
