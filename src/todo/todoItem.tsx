b96771d4741a5174f7ab5e53bdc67f4222ca94f5
saragrawal <saragrawal@expedia.com>
2020-06-18 20:54:49 +0530

    Code refactoring

--- src/todo/todoItem.tsx
+++ src/todo/todoItem.tsx
@@ -1,8 +1,5 @@
 import * as React from 'react';
-import CustomButton from './buttonOnclickProps';
-import { InputGroup, Form } from 'react-bootstrap';
-import { Icon } from 'react-icons-kit'
-import { cross } from 'react-icons-kit/icomoon/cross';
+import TodoItemView from './todoItemView';
 
 export enum ItemStatus {
     Creation,
@@ -100,50 +97,4 @@ export default function TodoItem (props: TodoItemProps): React.ReactElement {
             completeButtonHidden = {completeButtonHidden}
         />
     )
-}
-
-interface TodoItemViewProps {
-    content: string,
-    onChange: Function,
-    addButtonDisabled: boolean,
-    addButtonClick: Function,
-    deleteButtonDisabled: boolean,
-    deleteButtonClick: Function,
-    completeButtonDisabled: boolean,
-    completeButtonClick: Function,
-    addButtonHidden: boolean,
-    editButtonHidden: boolean,
-    editButtonDisabled: boolean,
-    editButtonClick: Function,
-    completeButtonHidden: boolean
-}
-
-function TodoItemView(props: TodoItemViewProps) {
-
-    return (
-        <li className="item">
-            <div className="item-view">
-                <InputGroup>
-                    <Form.Control
-                        id="itemText" 
-                        type="text" 
-                        className="mr-3"
-                        value={props.content} 
-                        onChange={e => props.onChange(e.target.value)}
-                        placeholder="Todo Item"
-                    />
-                    {/* <input id="itemText" type="text" value={props.content} onChange={e => props.onChange(e.target.value)} /> */}
-                    {/* <input type="checkbox" id={props.content} value={props.content} onClick={() => props.addButtonClick()}/>
-                    <label htmlFor={props.content} onChange={e => props.onChange(e.target.value)} /> */}
-                    <InputGroup.Append> 
-                        <CustomButton label="Add" isHidden={props.addButtonHidden} isDisabled={props.addButtonDisabled} clickAction={() => props.addButtonClick()} buttonVariant="primary" />
-                        <CustomButton label="Edit" isHidden={props.editButtonHidden} clickAction={() => props.editButtonClick()} buttonVariant="primary" />
-                        <CustomButton label="Complete" isHidden={props.completeButtonHidden} isDisabled={props.completeButtonDisabled} clickAction={(e) => props.completeButtonClick(e)} buttonVariant="dark" />
-                        <CustomButton label={<Icon icon={cross} />} isDisabled={props.deleteButtonDisabled} clickAction={() => props.deleteButtonClick()} buttonVariant="light" />
-                        
-                    </InputGroup.Append>
-                </InputGroup>
-            </div>
-        </li>
-    );
 }
